cc0f7f2ea9c0a3230a4eed2b95935fce69f057e1
Kyle Doud <krdoud@gmail.com>
2023-11-26 17:24:45 -0600

    switch to yarn and add filler bingo categories

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,15 @@
-import {Grid} from '@mui/joy';
+import { Grid } from '@mui/joy';
 import Item from '@mui/joy/Grid';
 
-
+const bingo = 'BINGO'.split('');
+const categories = 'this 25 word list represents the categories of a bingo game 12 FREE 14 15 16 17 18 19 20 21 22 23 24 25'.split(' ');
+const fullGridText = bingo.concat(categories);
 export default function Home() {
-    return <Grid container spacing={2}>
-    <Grid xs={8}>
-      <Item>xs=8</Item>
-    </Grid>
-    <Grid xs={4}>
-      <Item>xs=4</Item>
-    </Grid>
-    <Grid xs={4}>
-      <Item>xs=4</Item>
-    </Grid>
-    <Grid xs={8}>
-      <Item>xs=8</Item>
-    </Grid>
+  return <Grid container spacing={2}>
+    {fullGridText.map(text => {
+      return (<Grid xs={2.4}>
+        <Item>{text}</Item>
+      </Grid>)
+    })}
   </Grid>
-  }
+}
